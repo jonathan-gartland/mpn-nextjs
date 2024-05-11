87e05721e783de2e85d887be4f42664d2a0e12a8
jonathan-gartland <jonathan-gartland@gmail.com>
2024-05-10 21:49:59 -0400

    add passing placeholder tests

---
+++ tests/__tests__/mpn-lookup/passing.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe("MPN Lookup", () => {
+  test('should return "Hello World!"', () => {
+    expect("Hello World!").toBe("Hello World!");
+  });
+});
