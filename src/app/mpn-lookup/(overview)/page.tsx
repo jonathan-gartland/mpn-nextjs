ca1dc8d3bc22707c3f746613e687efd4c4de37fc
jonathan-gartland <jonathan-gartland@gmail.com>
2024-05-20 12:29:52 -0400

    refactor quantitray page, working buyt minimal style, save state

--- src/app/mpn-lookup/(overview)/page.tsx
+++ src/app/mpn-lookup/(overview)/page.tsx
@@ -11,23 +11,21 @@ export default async function Page() {
             MPN Lookup
           </h1>
           <br />
-          <p>
-            An automated lookup app so you don&apos;t need the relevant
-            PDF&apos;s for the following water quality test systems:
-            <br />
-            <br />
-            <div className="grid">
-              <div>
-                <h1>QuantiTray&reg;</h1>
-              </div>
-              <div>
-                <h1>QuantiTray2000&reg;</h1>
-              </div>
-              <div>
-                <h1>Legiolert&reg;</h1>
-              </div>
+          An automated lookup app so you don&apos;t need the relevant PDF&apos;s
+          for the following water quality test systems:
+          <br />
+          <br />
+          <div className="grid">
+            <div>
+              <h1>QuantiTray&reg;</h1>
             </div>
-          </p>
+            <div>
+              <h1>QuantiTray2000&reg;</h1>
+            </div>
+            <div>
+              <h1>Legiolert&reg;</h1>
+            </div>
+          </div>
         </div>
         <div></div>
       </div>
--- src/app/mpn-lookup/quantitray/page.tsx
+++ src/app/mpn-lookup/quantitray/page.tsx
@@ -21,40 +21,63 @@ export default function Mpn() {
     }
 
     return (
-      <div>
-        <h5 data-testid={"qt-mpn-val"}>MPN: {mpnList[1]}</h5>
-        <label data-testid={"qt-conf-label"}>95% Confidence Range</label>
-        <br />
-        <span>
-          Low: {mpnList[0]} -- High: {mpnList[2]}
-        </span>
+      <div className="grid grid-cols-2 gap-1 mb-6 w-24">
+        <div>MPN:</div>
+        <div>{mpnList[1]}</div>
+      </div>
+    );
+  }
+
+  function mpn95conf(inCount: number) {
+    let mpnList: any[] = [];
+    if (inCount >= 0 && inCount <= 51) {
+      mpnList = getQtMpn(inCount);
+    }
+    // if (mpnList.length === 0) {
+    //   return (
+    //     <label data-testid={"empty-value-label"}>
+    //       Enter valid value please!
+    //     </label>
+    //   );
+    // }
+
+    return (
+      <div className="grid grid-cols-2 gap-4 mb-6 w-24">
+        <div>High: </div>
+        <div>{mpnList[2]}</div>
+        <div>Low:</div>
+        <div>{mpnList[0]}</div>
       </div>
     );
   }
 
   return (
-    <>
-      <label>
-        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-          QuantiTray&reg;
-        </h1>
-        <br />
-        <br />
-        {/* eslint-disable-next-line max-len */}
-        Enter Positive Well Count:{" "}
-        <input
-          name={"QTinput"}
-          id={"qtinput"}
-          value={inCount}
-          onChange={(e) => setInCount(parseInt(e.target.value))}
-          type="number"
-          min="0"
-          max="51"
-          data-testid={"qt-input-dataid"}
-        />
-        <br />
+    <div className="w-1/2">
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        QuantiTray&reg; MPN
+      </h1>
+      <br />
+      <div className="grid grid-cols-2 gap-4 mb-6 ">
+        <div>
+          Enter Positive Well Count:&emsp;
+          <input
+            className="rounded-md w-12 px-1"
+            name={"QTinput"}
+            id={"qtinput"}
+            value={inCount}
+            onChange={(e) => setInCount(parseInt(e.target.value))}
+            type="number"
+            min="0"
+            max="51"
+            data-testid={"qt-input-dataid"}
+          />
+        </div>
         {<div>{mpn(inCount)}</div>}
-      </label>
-    </>
+        <div>
+          <label data-testid={"qt-conf-label"}>95% Confidence Range</label>
+        </div>
+        {<div>{mpn95conf(inCount)}</div>}
+      </div>
+    </div>
   );
 }
