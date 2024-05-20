a408b96703439a936fca9e4d648a6ede9400148a
jonathan-gartland <jonathan-gartland@gmail.com>
2024-05-20 17:16:33 -0400

    first draft app done, save state

--- src/app/mpn-lookup/about/page.tsx
+++ src/app/mpn-lookup/about/page.tsx
@@ -14,9 +14,14 @@ export default function AboutForm() {
       }}
       className={`${lusitana.className} text-lg`}
     >
-      <div style={{ textAlign: "left" }} data-testid={"about-text-id"}>
+      <div
+        className="w-1/2"
+        style={{ textAlign: "left" }}
+        data-testid={"about-text-id"}
+      >
         <p>
-          Some folks work in water testing facilities that utilize the Idexx{" "}
+          Some folks work in water testing facilities that utilize the
+          Idexx&nbsp;
           <a
             data-testid={"qtlink"}
             style={{ color: "teal" }}
@@ -37,8 +42,9 @@ export default function AboutForm() {
             QuantiTray2000&reg;
           </a>
           , which is the largest one.
-          <br />
-          <br />
+        </p>
+        <br />
+        <p>
           Allegedly created to be helpful in the tedious process of looking up
           the MPN related to positive flourescing cells in an incubated sample,
           the{" "}
@@ -48,26 +54,51 @@ export default function AboutForm() {
             href="https://www.idexx.com/en/water/resources/mpn-generator/"
           >
             MPN Generator
+          </a>
+          &nbsp;was created.
+        </p>
+        <br />
+        <p>
+          If the story is being recalled correctly, this desktop solution was
+          written so one of the software engineers could practice building a
+          desktop application using Delphi-C. &nbsp;The MPN Generator can
+          inexplicably still be&nbsp;
+          <i>
+            downloaded and installed on your computer as a Windows&trade;
+            desktop application today.
+          </i>
+        </p>
+        <br />
+        <p>
+          While the previous detail may cause the reader to ask some questions
+          related to why the above is the current state of affairs, your author
+          simply decided to make a{" "}
+          <a
+            style={{ color: "teal" }}
+            href="https://github.com/jonathan-gartland/mpn-lookup"
+          >
+            useful javascript library
           </a>{" "}
-          was created.
-          <br />A Desktop solution to be installed on your Windows machine...
-          well you can read about it at that link for the revised version, but a
-          tl;dr summary would be that this particular software was written
-          without any planning, research, or demand, so that one engineer could
-          practice with Delphi C back in the 1990s. This has been referred to in
-          the past as &quot;Deanware&quot; at this particular sales and
-          manufacturing organization. A decade after the buggy implementation
-          was released to the public - The confidence range values were coded in
-          backwards, the save drive was hardcoded as E: despite many people not
-          having an E: drive set up, etc...- the types of problems requirements
-          gathering, testing and debugging would have caught/prevented. Anyway
-          this is getting way too long so to cut short my justification for
-          writing this app other than learning to use React. This is a usable
-          implementation of that prior desktop app that needlessly
-          overcomplicates the task it is supposed to be helping with.
-          <div data-testid={"last-sentence-testid"}>
-            Hope this is useful to the three of you that need it.
-          </div>
+          (and{" "}
+          <a
+            style={{ color: "teal" }}
+            href="https://github.com/jonathan-gartland/mpn-helpers"
+          >
+            one in Python as well
+          </a>{" "}
+          ) to give the user the MPN value based on the simple input provided,
+          the integer values of the incubated cells that show positive signs of
+          contamination. If these tools were needed for a Jupyter notebook or an
+          application they would be readily available via npm or pip. This
+          seemed slightly more flexible and practical than the existing
+          solution. If this application is useful to three or four people out
+          there have fun, it will always be freely available on github.
+        </p>
+        <br />
+        <p>
+          This app is simply a demo using the library to show how it could be
+          implemented in a web app. I wanted more practice using React and
+          NextJS, and this seemed like an easy task to do it with.
         </p>
       </div>
     </div>
--- src/app/mpn-lookup/quantitray2000/page.tsx
+++ src/app/mpn-lookup/quantitray2000/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { getQt2KMpn } from "mpn-lookup/mpn.lookup";
+import { lusitana } from "@/app/ui/fonts";
 
 export default function QTLForm() {
   const [in2KL, setIn2KL] = useState(0);
@@ -14,53 +15,70 @@ export default function QTLForm() {
     }
 
     return (
-      <div>
-        <h5 data-testid={"qt2k-mpn-val"}>MPN: {mpn2KList[1]}</h5>
-        <label data-testid={"qt2k-conf-label"} style={{ color: "#61dafb" }}>
-          95% Confidence Range
-        </label>
-        <br />
-        <span>
-          Low: {mpn2KList[0]} -- High: {mpn2KList[2]}
-        </span>
+      <div className="grid grid-cols-2 gap-6 mb-6 w-24 text-red-600 text-xl">
+        <div>MPN:</div>
+        <div className="text-red-600 font-bold text-xl">{mpn2KList[1]}</div>
+      </div>
+    );
+  }
+
+  function mpn2K95conf(in2KL: number, in2KS: number) {
+    let mpn2KList: any = [];
+    if (in2KL >= 0 && in2KL <= 49 && in2KS >= 0 && in2KS <= 48) {
+      mpn2KList = getQt2KMpn(in2KL, in2KS);
+    }
+
+    return (
+      <div className="grid grid-cols-2 gap-4 mb-6 w-24">
+        <div>High: </div>
+        <div>{mpn2KList[2]}</div>
+        <div>Low:</div>
+        <div>{mpn2KList[0]}</div>
       </div>
     );
   }
 
   return (
-    <div>
-      <label>
-        <label id={"qt2k-title-label"} style={{ color: "#61dafb" }}>
-          QuantiTray2000&reg;
-        </label>
-        <br />
-        <br />
-        {/* eslint-disable-next-line max-len */}
-        Enter Large Well Count:{" "}
-        <input
-          name={"QT2KLinput"}
-          id={"qt2klinput"}
-          value={in2KL}
-          onChange={(e) => setIn2KL(parseInt(e.target.value))}
-          type="number"
-          min="0"
-          max="49"
-        />
-        <br />
-        {/* eslint-disable-next-line max-len */}
-        Enter Small Well Count:{" "}
-        <input
-          name={"QT2KSinput"}
-          id={"qt2ksinput"}
-          value={in2KS}
-          onChange={(e) => setIn2KS(parseInt(e.target.value))}
-          type="number"
-          min="0"
-          max="48"
-        />
-        <br />
-        {<div>{mpn2K(in2KL, in2KS)}</div>}
-      </label>
+    <div className={`${lusitana.className} w-1/2`}>
+      <h1 className="mb-4 text-xl md:text-2xl text-blue-600">
+        QuantiTray2000&reg; MPN
+      </h1>
+      <br />
+      <div className="grid grid-cols-2 gap-4 mb-6 ">
+        <div className={"text-xl"}>
+          <div>
+            Enter Large Well Count:&emsp;
+            <input
+              className="rounded-md w-12 px-1"
+              name={"QT2KLinput"}
+              id={"qt2klinput"}
+              value={in2KL}
+              onChange={(e) => setIn2KL(parseInt(e.target.value))}
+              type="number"
+              min="0"
+              max="49"
+            />
+          </div>
+          <div>
+            Enter Small Well Count:&emsp;
+            <input
+              className="rounded-md w-12 px-1"
+              name={"QT2KSinput"}
+              id={"qt2ksinput"}
+              value={in2KS}
+              onChange={(e) => setIn2KS(parseInt(e.target.value))}
+              type="number"
+              min="0"
+              max="48"
+            />
+          </div>
+        </div>
+        <div>{mpn2K(in2KL, in2KS)}</div>
+        <div className="text-xl">
+          <label data-testid={"qt-conf-label"}>95% Confidence Range</label>
+        </div>
+        {<div className="text-lg">{mpn2K95conf(in2KL, in2KS)}</div>}
+      </div>
     </div>
   );
 }
