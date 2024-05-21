535b8b13247e288358f0e281f9a751dbd8ff060a
jonathan-gartland <jonathan-gartland@gmail.com>
2024-05-21 11:55:25 -0400

    add dummy test

--- tests/__tests__/mpn-lookup/passing.test.ts
+++ tests/__tests__/mpn-lookup/passing.test.ts
@@ -4,4 +4,8 @@ describe("MPN Lookup", () => {
   test('should return "Hello World!"', () => {
     expect("Hello World!").toBe("Hello World!");
   });
+
+  test("should return true", () => {
+    expect(1).toEqual(2 - 1);
+  });
 });
