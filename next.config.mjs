c8097be6bcc104f87b80acbfa772000b4cefa842
jonathan-gartland <jonathan-gartland@gmail.com>
2024-05-21 12:38:47 -0400

    fix url

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/mpn-lookup",
+        destination: "/mpn-lookup/",
         permanent: true,
       },
     ];
